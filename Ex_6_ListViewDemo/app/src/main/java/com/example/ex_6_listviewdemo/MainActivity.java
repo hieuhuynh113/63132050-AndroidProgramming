5969fd1719586c31940b82355202c91e1fa9bd88
hieuhuynh113 <hieu01645369512@gmail.com>
2024-03-13 21:10:12 +0700

    Update Huongdan1

--- Ex_6_ListViewDemo/app/src/main/java/com/example/ex_6_listviewdemo/MainActivity.java
+++ Ex_6_ListViewDemo/app/src/main/java/com/example/ex_6_listviewdemo/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.ex_6_listviewdemo;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +20,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Hiển thị dữ liệu lên ListView
-        //B1:
+        //B1: Cần có dữ liêu
+        //Cần biển phù hợp để có dữ liệu
+        ArrayList<String> dsTenTinhThanhVN;
+        dsTenTinhThanhVN = new ArrayList<String>();
+        //Thêm dữ liệu ở đây
+        dsTenTinhThanhVN.add("Hà Nội");
+        dsTenTinhThanhVN.add("Thành phố Hồ Chí Minh");
+        dsTenTinhThanhVN.add("Đồng Nai");
+        dsTenTinhThanhVN.add("Bình Thuận");
+        dsTenTinhThanhVN.add("Ninh Thuận");
+        dsTenTinhThanhVN.add("Nha Trang");
 
+        //B2: Tạo adapter
+        ArrayAdapter<String> adapterTinhThanh;
+        adapterTinhThanh = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dsTenTinhThanhVN);
+
+        //B3: Gắn vào điều khiển hiển thị ListView
+        //3.1: Tìm
+        ListView lvTenTinhThanh = findViewById(R.id.lvDanhSachTT);
+        //3.2: Gắn
+        lvTenTinhThanh.setAdapter(adapterTinhThanh);
+        //3.3: Lắng nghe và xử lý sự kiện user tương tác
 
     }
 }
