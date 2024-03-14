0e87a15f4ca25266fe0f8253bf2af97c7368795f
hieuhuynh113 <hieu01645369512@gmail.com>
2024-03-14 19:56:58 +0700

    Complete Ex6

--- Ex_6_ListViewDemo/app/src/main/java/com/example/ex_6_listviewdemo/MainActivity.java
+++ Ex_6_ListViewDemo/app/src/main/java/com/example/ex_6_listviewdemo/MainActivity.java
@@ -1,47 +1,40 @@
 package com.example.ex_6_listviewdemo;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<String> dsTenCacTinhThanh = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dsTenCacTinhThanh.add("Hà Nội");
+        dsTenCacTinhThanh.add("Thành phố Hồ Chí Minh");
+        dsTenCacTinhThanh.add("Khánh Hòa");
+        dsTenCacTinhThanh.add("Đà Nẵng");
+        dsTenCacTinhThanh.add("Lâm Đồng");
 
-        //Hiển thị dữ liệu lên ListView
-        //B1: Cần có dữ liêu
-        //Cần biển phù hợp để có dữ liệu
-        ArrayList<String> dsTenTinhThanhVN;
-        dsTenTinhThanhVN = new ArrayList<String>();
-        //Thêm dữ liệu ở đây
-        dsTenTinhThanhVN.add("Hà Nội");
-        dsTenTinhThanhVN.add("Thành phố Hồ Chí Minh");
-        dsTenTinhThanhVN.add("Đồng Nai");
-        dsTenTinhThanhVN.add("Bình Thuận");
-        dsTenTinhThanhVN.add("Ninh Thuận");
-        dsTenTinhThanhVN.add("Nha Trang");
-
-        //B2: Tạo adapter
-        ArrayAdapter<String> adapterTinhThanh;
-        adapterTinhThanh = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dsTenTinhThanhVN);
-
-        //B3: Gắn vào điều khiển hiển thị ListView
-        //3.1: Tìm
+        ArrayAdapter<String> adapterTinhThanh = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dsTenCacTinhThanh);
         ListView lvTenTinhThanh = findViewById(R.id.lvDanhSachTT);
-        //3.2: Gắn
         lvTenTinhThanh.setAdapter(adapterTinhThanh);
-        //3.3: Lắng nghe và xử lý sự kiện user tương tác
 
+        lvTenTinhThanh.setOnItemClickListener(BoLangNgheSuKienVaXL);
     }
+
+    AdapterView.OnItemClickListener BoLangNgheSuKienVaXL = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String strTinhThanhChon = dsTenCacTinhThanh.get(position);
+            Toast.makeText(MainActivity.this, strTinhThanhChon, Toast.LENGTH_LONG).show();
+        }
+    };
 }
