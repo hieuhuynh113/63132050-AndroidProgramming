69dbff79793aca0ffbd61e3fe943b30351e71b31
hieuhuynh113 <hieu01645369512@gmail.com>
2024-03-25 17:35:25 +0700

    lan 14

--- Cau3_Introduce/app/src/main/java/ntu_63132050/cau3_introduce/MainActivity.java
+++ Cau3_Introduce/app/src/main/java/ntu_63132050/cau3_introduce/MainActivity.java
@@ -1,11 +1,11 @@
 package ntu_63132050.cau3_introduce;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void facebookLink(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/profile.php?id=100078969199950"));
+        startActivity(intent);
+    }
+
+    public void githupLink(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.githup.com/hieuhuynh113"));
+        startActivity(intent);
+    }
+
+    public void instagramLink(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/hiuthubar_/"));
+        startActivity(intent);
+    }
+    public void backToMain (View v){
+        finish();
     }
 }
 
